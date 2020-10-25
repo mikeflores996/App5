9ecbe478b3ae644314bd97c0d350e6cfb29fb010
Jose Miguel <mike_trko@hotmail.com>
2020-10-25 00:08:17 -0500

    se agrego splashscreen y se estan modificando pantallas

---
+++ App5/App5/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using App5.NewFolder3;
+using App5.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App5
+{
+    public partial class App : Application
+    {
+        public Login Login { get; private set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Start();
+            
+        }
+
+        protected override void OnStart()
+        {
+            Login = new Login();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
